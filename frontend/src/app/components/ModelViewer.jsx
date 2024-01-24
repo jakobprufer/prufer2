4c0a032024954b5bfa775b8f4ca97e0dc2973ac6
Jakob Prufer <jakobo.pruefer@gmail.com>
2024-01-24 12:05:04 +0100

    commented out model-viewer

--- frontend/src/app/components/ModelViewer.jsx
+++ frontend/src/app/components/ModelViewer.jsx
@@ -1,43 +1,43 @@
-"use client";
-import React, { useEffect, useState } from "react";
-import "@google/model-viewer";
-import "../components/ModelViewer.css";
-import { useNavStore } from "../store/navStore";
+// "use client";
+// import React, { useEffect, useState } from "react";
+// import "@google/model-viewer";
+// import "../components/ModelViewer.css";
+// import { useNavStore } from "../store/navStore";
 
-const ModelViewer = ({ scrollY }) => {
-  const inside = useNavStore((state) => state.inside);
+// const ModelViewer = ({ scrollY }) => {
+//   const inside = useNavStore((state) => state.inside);
 
-  const [rotation, setRotation] = useState(0);
+//   const [rotation, setRotation] = useState(0);
 
-  useEffect(() => {
-    // Calculate the rotation based on the passed scrollY prop
-    const newRotation = (scrollY / 10) % 360;
-    setRotation(newRotation);
-  }, [scrollY]); // Dependency array ensures this runs only when scrollY changes
+//   useEffect(() => {
+//     // Calculate the rotation based on the passed scrollY prop
+//     const newRotation = (scrollY / 10) % 360;
+//     setRotation(newRotation);
+//   }, [scrollY]); // Dependency array ensures this runs only when scrollY changes
 
-  return (
-    <div className={`modelCont ${inside ? "inside" : null}`}>
-      <model-viewer
-        src="/glaceModel.glb" // Replace with your model path
-        alt="A 3D model"
-        auto-rotate
-        camera-controls
-        style={{
-          width: "600px",
-          height: "600px",
-          background: "00000000",
-        }}
-        camera-orbit={`-${rotation}deg 80deg 4m`}
-        loading="eager"
-        disable-zoom
-        background-color="transparent"
-        interaction-prompt="none"
-        shadow-intensity="0.5"
-      >
-        <div id="poster" slot="poster"></div>
-      </model-viewer>
-    </div>
-  );
-};
+//   return (
+//     <div className={`modelCont ${inside ? "inside" : null}`}>
+//       <model-viewer
+//         src="/glaceModel.glb" // Replace with your model path
+//         alt="A 3D model"
+//         auto-rotate
+//         camera-controls
+//         style={{
+//           width: "600px",
+//           height: "600px",
+//           background: "00000000",
+//         }}
+//         camera-orbit={`-${rotation}deg 80deg 4m`}
+//         loading="eager"
+//         disable-zoom
+//         background-color="transparent"
+//         interaction-prompt="none"
+//         shadow-intensity="0.5"
+//       >
+//         <div id="poster" slot="poster"></div>
+//       </model-viewer>
+//     </div>
+//   );
+// };
 
-export default ModelViewer;
+// export default ModelViewer;
